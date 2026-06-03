ac5fedfe24dba93aa2c12b8ef1995638bdfb4905
Rodrigo-ACR <rodrigoantonioconcharomero@gmail.com>
2026-06-02 21:08:13 -0400

    feat: agregar tests unitarios de usuarios y fix H2

--- usuarios/src/test/java/com/smartlogix/usuarios/UsuariosApplicationTests.java
+++ usuarios/src/test/java/com/smartlogix/usuarios/UsuariosApplicationTests.java
@@ -2,12 +2,13 @@ package com.smartlogix.usuarios;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations = "classpath:application-test.properties")
 class UsuariosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
-
+    @Test
+    void contextLoads() {
+    }
 }
