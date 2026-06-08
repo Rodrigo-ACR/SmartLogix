1108eac01458407b19681d8f029caa10591d7c38
Rodrigo-ACR <rodrigoantonioconcharomero@gmail.com>
2026-06-08 02:38:21 -0400

    fix: corregir MockBean deprecado y sincronizar pom.xml de usuarios

--- inventario/src/test/java/com/smartlogix/inventario/controller/ProductoControllerTest.java
+++ inventario/src/test/java/com/smartlogix/inventario/controller/ProductoControllerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -24,7 +24,7 @@ class ProductoControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private ProductoService service;
 
     @Autowired
